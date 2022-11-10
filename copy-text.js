ce72714c1169eac25a67be8e5374207fd070cdcd
Zsofia Deak <zsofia.deak@supercharge.io>
2022-11-10 16:43:39 +0100

    fix: fixing copy issue

--- copy-text.js
+++ copy-text.js
@@ -1,7 +1,14 @@
-function copyText() {
-
-    var Text = document.getElementById("box");
-    Text.select();
-
-    navigator.clipboard.writeText(Text.value);
-}
+function copy() {
+    if (document.selection) {
+      var range = document.body.createTextRange();
+      range.moveToElementText(document.getElementById('generated'));
+      range.select().createTextRange();
+      document.execCommand("copy");
+    } else if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNode(document.getElementById('generated'));
+      window.getSelection().addRange(range);
+      document.execCommand("copy");
+      alert("Text has been copied, now paste in the text-area")
+    }
+  }
