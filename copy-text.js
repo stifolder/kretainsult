2a232c1c5db948f20be9fd8d545a0893f57d59a5
Zsofia Deak <zsofia.deak@supercharge.io>
2022-11-10 16:49:47 +0100

    feat: change copy to copy all text under the box class

--- copy-text.js
+++ copy-text.js
@@ -1,14 +1,14 @@
 function copy() {
     if (document.selection) {
       var range = document.body.createTextRange();
-      range.moveToElementText(document.getElementById('generated'));
+      range.moveToElementText(document.getElementById('box'));
       range.select().createTextRange();
       document.execCommand("copy");
     } else if (window.getSelection) {
       var range = document.createRange();
-      range.selectNode(document.getElementById('generated'));
+      range.selectNode(document.getElementById('box'));
       window.getSelection().addRange(range);
       document.execCommand("copy");
-      alert("Text has been copied, now paste in the text-area")
+      alert("Kimásolva!")
     }
   }
